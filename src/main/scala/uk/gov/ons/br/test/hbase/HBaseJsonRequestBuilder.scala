151646f7deb8e5c6babbcec5c87787c8b7a234b5
Nigel Perkins <nigelhp@yahoo.co.uk>
2019-01-15 12:04:29 +0000

    REG-2369: Add support for stubbing HBase PUT requests

--- src/main/scala/uk/gov/ons/br/test/hbase/HBaseJsonRequestBuilder.scala
+++ src/main/scala/uk/gov/ons/br/test/hbase/HBaseJsonRequestBuilder.scala
@@ -1,19 +1,28 @@
 package uk.gov.ons.br.test.hbase
 
 import com.github.tomakehurst.wiremock.client.MappingBuilder
-import com.github.tomakehurst.wiremock.client.WireMock.{equalTo, get, urlEqualTo}
-import play.api.http.HeaderNames.ACCEPT
+import com.github.tomakehurst.wiremock.client.WireMock.{equalTo, get, put, urlEqualTo}
+import play.api.http.HeaderNames.{ACCEPT, CONTENT_TYPE}
 import play.mvc.Http.MimeTypes.JSON
 import uk.gov.ons.br.test.http.BasicAuthentication
 
 trait HBaseJsonRequestBuilder extends BasicAuthentication {
   def getHBaseJson(namespace: String, tableName: String, rowKey: String,
                    auth: Authorization = Authorization.DummyAuthorization): MappingBuilder =
-    get(urlEqualTo(getUrlFor(namespace, tableName, rowKey))).
+    get(urlEqualTo(urlForRow(namespace, tableName, rowKey))).
       withHeader(ACCEPT, equalTo(JSON)).
       withHeader(Authorization.HeaderName, equalTo(Authorization.value(auth)))
 
-  private def getUrlFor(namespace: String, tableName: String, rowKey: String): String =
+  def checkedPutHBaseJson(namespace: String, tableName: String, rowKey: String,
+                          auth: Authorization = Authorization.DummyAuthorization): MappingBuilder =
+    putHBaseJson(urlForRow(namespace, tableName, rowKey).concat("/?check=put"), auth)
+
+  private def putHBaseJson(url: String, auth: Authorization): MappingBuilder =
+    put(urlEqualTo(url)).
+      withHeader(CONTENT_TYPE, equalTo(JSON)).
+      withHeader(Authorization.HeaderName, equalTo(Authorization.value(auth)))
+
+  private def urlForRow(namespace: String, tableName: String, rowKey: String): String =
     s"/$namespace:$tableName/$rowKey"
 }
 
--- src/test/scala/uk/gov/ons/br/test/hbase/HBaseJsonRequestBuilderSpec.scala
+++ src/test/scala/uk/gov/ons/br/test/hbase/HBaseJsonRequestBuilderSpec.scala
@@ -1,9 +1,9 @@
 package uk.gov.ons.br.test.hbase
 
-import com.github.tomakehurst.wiremock.http.RequestMethod.GET
+import com.github.tomakehurst.wiremock.http.RequestMethod.{GET, PUT}
 import com.github.tomakehurst.wiremock.matching.{EqualToPattern, MultiValuePattern}
 import org.scalatest.{FreeSpec, Matchers}
-import play.api.http.HeaderNames.{ACCEPT, AUTHORIZATION}
+import play.api.http.HeaderNames.{ACCEPT, AUTHORIZATION, CONTENT_TYPE}
 import play.api.http.MimeTypes.JSON
 import uk.gov.ons.br.test.hbase.HBaseJsonRequestBuilderSpec._
 
@@ -18,6 +18,17 @@ class HBaseJsonRequestBuilderSpec extends FreeSpec with Matchers {
       request.getMethod shouldBe GET
       request.getUrl shouldBe s"/$Namespace:$TableName/$RowKey"
       request.getHeaders.get(ACCEPT) shouldBe MultiValuePattern.of(new EqualToPattern(JSON))
+      request.getHeaders.get(AUTHORIZATION) shouldBe MultiValuePattern.of(new EqualToPattern(AuthHeaderValue))
+    }
+
+    "that represents a HBase REST checked update" in {
+      val mappingBuilder = HBaseJsonRequestBuilder.checkedPutHBaseJson(Namespace, TableName, RowKey, BasicAuthentication)
+
+      val request = mappingBuilder.build().getRequest
+
+      request.getMethod shouldBe PUT
+      request.getUrl shouldBe s"/$Namespace:$TableName/$RowKey/?check=put"
+      request.getHeaders.get(CONTENT_TYPE) shouldBe MultiValuePattern.of(new EqualToPattern(JSON))
       request.getHeaders.get(AUTHORIZATION) shouldBe MultiValuePattern.of(new EqualToPattern(AuthHeaderValue))
     }
   }
